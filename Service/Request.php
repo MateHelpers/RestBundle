5dfbd4d69b579564f7f22b569f90899f15261cc9
Mohamed Radhi Guennichi <hoster07@gmail.com>
2017-10-05 11:24:07 +0200

    Update Request.php

--- Service/Request.php
+++ Service/Request.php
@@ -12,13 +12,16 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Exception\RuntimeException;
 
-class Request {
+abstract class Request {
 
 	/** @var RequestStack */
 	protected $requestStack;
 
 	/** @var Validator */
 	protected $validator;
+	
+	/** @var Dot */
+	protected $dotRequest;
 
 	/**
 	 * RequestService constructor.
@@ -28,7 +31,14 @@ class Request {
 	{
 		$this->requestStack = $requestStack;
 		$this->validator    = new Validator();
+		
+		$this->dotRequest   = $this->buildDotRequest();
 	}
+	
+	/**
+	 * @return array
+	 */
+	abstract protected function rules(): array;
 
 	/**
 	 * @param string $requestClass
@@ -36,19 +46,12 @@ class Request {
 	 * @return DotArray
 	 * @throws \Exception
 	 */
-	public function handleFromClass($requestClass, $group = null)
+	public function handle($group = null)
 	{
-		/** @var RequestInterface $requestInstance */
-		$request = new $requestClass();
-
-		if (!$request instanceof RequestInterface) {
-			throw new RuntimeException('Request class must be instance of ' . RequestInterface::class);
-		}
-
-		$rules = $request->rules();
+		$rules = $this->rules();
 
 		if ($group && !array_key_exists($group, $rules))
-			throw new HttpException(sprintf('Undefined %s group on %s class', $group, $requestClass));
+			throw new HttpException(sprintf('Undefined %s group', $group));
 
 		$validation = $this->validator->validate($this->getRequest()->all(), $group ? $rules[$group] : $rules);
 
@@ -57,7 +60,7 @@ class Request {
 			throw new BadRequestHttpException($error[0]);
 		}
 
-		return $this->getRequest();
+		return $this;
 	}
 
 	/**
@@ -67,12 +70,12 @@ class Request {
 	 */
 	public function get($key)
 	{
-		$requestDotArray = $this->getDotArrayRequest();
+		$request = $this->getRequest();
 
-		if (!$requestDotArray->has($key))
+		if (!$request->has($key))
 			throw new BadRequestHttpException("$key not found in the request");
 
-		return $requestDotArray->get($key);
+		return $request->get($key);
 	}
 
 	/**
@@ -81,21 +84,21 @@ class Request {
 	 */
 	public function getRequest()
 	{
-		return $this->getDotArrayRequest();
+		return $this->dotRequest;
 	}
 
 	/**
 	 * @return DotArray
 	 * @throws \Exception
 	 */
-	private function getDotArrayRequest(){
+	private function buildDotRequest(){
 		$content = $this->getCurrentRequest()->getContent();
 
 		if(empty($content)){
 			throw new BadRequestHttpException("Request content is empty");
 		}
 
-		return new DotArray(json_decode($content, true));
+		$this->dotRequest = new DotArray(json_decode($content, true));
 	}
 
 	/**
